fe044f8001c3d73a006733a0b36c94a4a4004fd8
lastforever457 <lastforever457@gmail.com>
2025-08-05 14:48:21 +0500

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
-import 'aframe';
-import 'ar.js';
-
 const App = () => {
     return (
-        <div className={'w-full h-screen'}>
-{/*@ts-ignore*/}
-            <a-scene embedded arjs="debugUIEnabled: false; detectionMode: mono_and_matrix; matrixCodeType: 4x4;">
-{/*@ts-ignore*/}
-                <a-marker preset="hiro">
-{/*@ts-ignore*/}
-                    <a-entity gltf-model="/pojarni123.glb" position="-0.75 0.5 -2"></a-entity>
-{/*@ts-ignore*/}
-                </a-marker>
-{/*@ts-ignore*/}
-                <a-entity camera></a-entity>
-{/*@ts-ignore*/}
-            </a-scene>
-        </div>
-    );
+        // @ts-ignore
+        <a-scene
+            embedded
+            arjs="sourceType: webcam; debugUIEnabled: false;"
+        >
+            {/*@ts-ignore*/}
+            <a-marker preset="hiro">
+                {/*@ts-ignore*/}
+                <a-entity
+                    gltf-model="url(/pojarni123.glb)"
+                    scale="0.5 0.5 0.5"
+                    rotation="0 180 0"
+                />
+                {/*@ts-ignore*/}
+            </a-marker>
+            {/*@ts-ignore*/}
+            <a-entity camera/>
+            {/*@ts-ignore*/}
+        </a-scene>
+    )
 };
 
 export default App;
