ed68772c2d74156039d48a920d89ce2b1cfa75e7
lastforever457 <lastforever457@gmail.com>
2025-08-06 09:59:25 +0500

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,28 @@ const App = () => {
     return (
         // @ts-ignore
         <a-scene
+            vr-mode-ui="enabled: false;"
+            renderer="logarithmicDepthBuffer: true;"
+            arjs="trackingMethod: best; sourceType: webcam; debugUIEnabled: false;"
             embedded
-            arjs="sourceType: webcam; debugUIEnabled: false;"
         >
             {/*@ts-ignore*/}
-            <a-marker preset="hiro">
+            <a-marker
+                type="pattern"
+                url={'/marker.patt'}
+            >
                 {/*@ts-ignore*/}
                 <a-entity
-                    gltf-model="url(/pojarni123.glb)"
-                    scale="0.5 0.5 0.5"
-                    rotation="0 180 0"
+                    gltf-model={`url(/pojarni123.glb)`}
+                    scale="0.1 0.1 0.1"
+                    position="0 0.5 0"
+                    rotation="-90 0 0"
+                    animation-mixer
                 />
                 {/*@ts-ignore*/}
             </a-marker>
             {/*@ts-ignore*/}
-            <a-entity camera/>
+            <a-entity camera></a-entity>
             {/*@ts-ignore*/}
         </a-scene>
     )
